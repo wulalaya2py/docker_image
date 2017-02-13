77191f1a97787c617fe2fc392944d0300472ae10
lyc <lyc@meitu.com>
2017-02-13 18:37:18 +0800

    cuda vnc 镜像支持拷贝和中文

--- cuda-vnc/cuda_v8.0/custom_vnc.sh
+++ cuda-vnc/cuda_v8.0/custom_vnc.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 echo $1
-echo $1
 echo $(ls /)
 echo $(ls /home)
 sed -i '0,/port=-1/{s/port=-1/port='"$1"'/}' /etc/xrdp/xrdp.ini
@@ -9,4 +8,5 @@ ldconfig
 # Remove VNC lock (if process already killed)
 rm /tmp/.X1-lock /tmp/.X11-unix/X1
 # Run VNC server with tail in the foreground
-vncserver :$(($1-5900)) -geometry 1280x800 -depth 24 && tail -F /root/.vnc/*.log
+vncserver :$(($1-5900)) -geometry 1920x1080 -depth 24 && sed -i '/=1/ a autocutsel -fork' /root/.vnc/xstartup
+vncserver -kill :$(($1-5900)) &&  vncserver :$(($1-5900)) -geometry 1920x1080 -depth 24 && tail -F /root/.vnc/*.log
