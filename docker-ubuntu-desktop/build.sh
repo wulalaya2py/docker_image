00487c1673623fe4a0d7bf8b77672d059fb41b80
lyc <lyc@meitu.com>
2017-02-27 18:57:46 +0800

    common torch 镜像

--- docker-ubuntu-desktop/build.sh
+++ docker-ubuntu-desktop/build.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
-sudo docker build -t queeno/ubuntu-desktop:cuda_14.04_custom -f Dockerfile.14.04_custom .
+sudo docker build -t 172.16.3.16:5000/lab/offical-instance:base -f Dockerfile.base .
+sudo docker push 172.16.3.16:5000/lab/offical-instance:base
+
+sudo docker build -t 172.16.3.16:5000/lab/offical-instance:common -f Dockerfile.common .
+sudo docker push 172.16.3.16:5000/lab/offical-instance:common
+
+sudo docker build -t 172.16.3.16:5000/lab/offical-instance:torch -f Dockerfile.torch .
+sudo docker push 172.16.3.16:5000/lab/offical-instance:torch
