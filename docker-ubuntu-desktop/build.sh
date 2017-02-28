753d21bc6244ed13f807f56fb3f7f74f3abfd124
lyc <lyc@meitu.com>
2017-02-28 08:38:43 +0800

    add theano and lasagne

--- docker-ubuntu-desktop/build.sh
+++ docker-ubuntu-desktop/build.sh
@@ -8,3 +8,9 @@ sudo docker push 172.16.3.16:5000/lab/offical-instance:common
 
 sudo docker build -t 172.16.3.16:5000/lab/offical-instance:torch -f Dockerfile.torch .
 sudo docker push 172.16.3.16:5000/lab/offical-instance:torch
+
+sudo docker build -t 172.16.3.16:5000/lab/offical-instance:theano -f Dockerfile.theano .
+sudo docker push 172.16.3.16:5000/lab/offical-instance:theano
+
+sudo docker build -t 172.16.3.16:5000/lab/offical-instance:lasagne -f Dockerfile.lasagne .
+sudo docker push 172.16.3.16:5000/lab/offical-instance:lasagne
