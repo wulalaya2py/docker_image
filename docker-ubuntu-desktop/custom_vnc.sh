65c4e50c6e96639c569340068b06945847b175a5
lyc <lyc@meitu.com>
2017-03-01 12:35:07 +0800

    update base vnc

--- docker-ubuntu-desktop/custom_vnc.sh
+++ docker-ubuntu-desktop/custom_vnc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo $1
+echo $1 $2
 #sed -i '0,/port=-1/{s/port=-1/port='"$1"'/}' /etc/xrdp/xrdp.ini
 #ldconfig
 rm /tmp/.X1-lock /tmp/.X11-unix/X1
-/usr/bin/vncserver :$(($1-5900)) -geometry 1920x1080 -depth 24 && tail -f /root/.vnc/*:$(($1-5900)).log
+/usr/bin/vncserver :$(($1-5900)) -geometry $2 -depth 24 && tail -f /root/.vnc/*:$(($1-5900)).log
