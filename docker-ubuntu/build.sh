1b168299c4e531c68774cabc3aa2e6d509986cd9
lyc <lyc@meitu.com>
2017-03-07 17:49:02 +0800

    更新 desktop torch 的 Dockerfile 以便适应train的重构

--- docker-ubuntu/build.sh
+++ docker-ubuntu/build.sh
@@ -2,3 +2,6 @@
 
 sudo docker build -t 172.16.3.16:5000/lab/offical-train:torch -f Dockerfile.torch .
 sudo docker push 172.16.3.16:5000/lab/offical-train:torch
+
+sudo docker build -t 172.16.3.16:5000/lab/offical-train:torch_test -f Dockerfile.torch_test .
+sudo docker push 172.16.3.16:5000/lab/offical-train:torch_test
--- docker-ubuntu/run_host_train.sh
+++ docker-ubuntu/run_host_train.sh
@@ -3,7 +3,6 @@
 echo $1
 echo $2
 echo $3
-echo $4
 
 cd $1
-/bin/bash $3 > $1/$2.txt
+/bin/bash $3 > $1/$2
